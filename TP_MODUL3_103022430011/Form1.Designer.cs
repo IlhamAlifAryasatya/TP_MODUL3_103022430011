8ef68dbe071ed08bce850601d91bdb7aa7ee5dfb
Ilham <ialif9693@gmail.com>
2026-03-09 01:11:13 +0700

    Menambahkan tampilan GUI kalkulator

--- TP_MODUL3_103022430011/Form1.Designer.cs
+++ TP_MODUL3_103022430011/Form1.Designer.cs
@@ -28,12 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            labelOutput = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn0 = new Button();
+            btnPlus = new Button();
+            btnEqual = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // labelOutput
+            // 
+            labelOutput.BorderStyle = BorderStyle.FixedSingle;
+            labelOutput.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelOutput.Location = new Point(221, 69);
+            labelOutput.Name = "labelOutput";
+            labelOutput.Size = new Size(216, 35);
+            labelOutput.TabIndex = 0;
+            labelOutput.Text = "0";
+            labelOutput.TextAlign = ContentAlignment.MiddleCenter;
+            labelOutput.Click += label1_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 48.550724F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 51.449276F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 66F));
+            tableLayoutPanel1.Controls.Add(btn1, 0, 0);
+            tableLayoutPanel1.Controls.Add(btn2, 1, 0);
+            tableLayoutPanel1.Controls.Add(btn3, 2, 0);
+            tableLayoutPanel1.Controls.Add(btn6, 2, 1);
+            tableLayoutPanel1.Controls.Add(btn5, 1, 1);
+            tableLayoutPanel1.Controls.Add(btn4, 0, 1);
+            tableLayoutPanel1.Controls.Add(btn7, 0, 2);
+            tableLayoutPanel1.Controls.Add(btn8, 1, 2);
+            tableLayoutPanel1.Controls.Add(btn9, 2, 2);
+            tableLayoutPanel1.Controls.Add(btnEqual, 2, 3);
+            tableLayoutPanel1.Controls.Add(btn0, 1, 3);
+            tableLayoutPanel1.Controls.Add(btnPlus, 0, 3);
+            tableLayoutPanel1.Location = new Point(221, 118);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 58F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            tableLayoutPanel1.Size = new Size(216, 224);
+            tableLayoutPanel1.TabIndex = 1;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // btn1
+            // 
+            btn1.Dock = DockStyle.Fill;
+            btn1.Location = new Point(3, 3);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(66, 48);
+            btn1.TabIndex = 0;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Dock = DockStyle.Fill;
+            btn2.Location = new Point(75, 3);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(71, 48);
+            btn2.TabIndex = 1;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Dock = DockStyle.Fill;
+            btn3.Location = new Point(152, 3);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(61, 48);
+            btn3.TabIndex = 2;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += button3_Click;
+            // 
+            // btn6
+            // 
+            btn6.Dock = DockStyle.Fill;
+            btn6.Location = new Point(152, 57);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(61, 48);
+            btn6.TabIndex = 3;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Dock = DockStyle.Fill;
+            btn5.Location = new Point(75, 57);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(71, 48);
+            btn5.TabIndex = 4;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Dock = DockStyle.Fill;
+            btn4.Location = new Point(3, 57);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(66, 48);
+            btn4.TabIndex = 5;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            btn7.Dock = DockStyle.Fill;
+            btn7.Location = new Point(3, 111);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(66, 52);
+            btn7.TabIndex = 6;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Dock = DockStyle.Fill;
+            btn8.Location = new Point(75, 111);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(71, 52);
+            btn8.TabIndex = 7;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Dock = DockStyle.Fill;
+            btn9.Location = new Point(152, 111);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(61, 52);
+            btn9.TabIndex = 8;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += button9_Click;
+            // 
+            // btn0
+            // 
+            btn0.Dock = DockStyle.Fill;
+            btn0.Location = new Point(75, 169);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(71, 52);
+            btn0.TabIndex = 10;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Dock = DockStyle.Fill;
+            btnPlus.Location = new Point(3, 169);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(66, 52);
+            btnPlus.TabIndex = 11;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            btnPlus.Click += button12_Click;
+            // 
+            // btnEqual
+            // 
+            btnEqual.Dock = DockStyle.Fill;
+            btnEqual.Location = new Point(152, 169);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(61, 52);
+            btnEqual.TabIndex = 9;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(labelOutput);
+            Name = "Form1";
             Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label labelOutput;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn0;
+        private Button btnPlus;
+        private Button btnEqual;
     }
 }
--- TP_MODUL3_103022430011/Form1.cs
+++ TP_MODUL3_103022430011/Form1.cs
@@ -6,5 +6,30 @@ namespace TP_MODUL3_103022430011
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
